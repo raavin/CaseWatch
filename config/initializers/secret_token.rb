7e3718c63755d105c2f2211dc0584055f792e47a
unknown <jkeenan@dc3.smhow.local>
2013-10-17 12:27:27 +1100

    First Commit with Devise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Smhow::Application.config.secret_token = 'f4b9b49f88108c246920d67e547dbe2ba61ed15ae99196986c69798c8b173f9b7c5159da1adc4faf766743c8ce580b6e12bd67052f9ec337ecabb956b3849825'
